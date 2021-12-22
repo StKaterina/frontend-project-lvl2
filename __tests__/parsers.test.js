a5a89966e4438513c620dfd410ee3291b14c5904
Ekaterina Stepanova <stepanovaeswork@gmail.com>
2021-12-22 12:27:11 +0500

    feat: new test

--- __tests__/parsers.test.js
+++ __tests__/parsers.test.js
@@ -1,31 +1,26 @@
-import { describe, test, expect } from '@jest/globals';
+import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
-import yaml from 'js-yaml';
 import { fileURLToPath } from 'url';
-import genDiff from '../src/parsers.js';
+import genDiff from '../src/index.js';
 
 // eslint-disable-next-line no-underscore-dangle
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
-
 const readFile = (filePath) => fs.readFileSync(getFixturePath(filePath), 'utf-8');
 
-describe('genDiff json', () => {
-  const jsonFile1 = JSON.parse(readFile('file1.json'));
-  const jsonFile2 = JSON.parse(readFile('file2.json'));
-  const jsonExpectedFile = readFile('jsonExpectedFile.txt');
-  test('comparison json files', () => {
-    expect(genDiff(jsonFile1, jsonFile2)).toEqual(jsonExpectedFile);
-  });
-});
+const valuesForTests = [
+  { a: 'file1.json', b: 'file2.json', expected: 'plainJsonExpected.txt' },
+  { a: 'file1.yml', b: 'file2.yml', expected: 'plainYmlExpected.txt' },
+  { a: 'file3.json', b: 'file4.json', expected: 'nestedExpected.txt' },
+  { a: 'file3.yml', b: 'file4.yml', expected: 'nestedExpected.txt' },
+];
 
-describe('genDiff yml', () => {
-  const ymlFile1 = yaml.load(readFile('file1.yml'));
-  const ymlFile2 = yaml.load(readFile('file2.yml'));
-  const ymlExpectedFile = readFile('ymlExpectedFile.txt');
-  test('comparison yml files', () => {
-    expect(genDiff(ymlFile1, ymlFile2)).toBe(ymlExpectedFile);
-  });
-});
+test.each(valuesForTests)(
+  'Comparing $a and $b',
+  ({ a, b, expected }) => {
+    const fileExpected = readFile(expected);
+    expect(genDiff(a, b)).toEqual(fileExpected);
+  },
+);
